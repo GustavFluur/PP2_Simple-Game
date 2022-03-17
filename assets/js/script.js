8e40189802cf227947016687564d855c947b5ff0
GustavFluur <gustavfluur@hotmail.com>
2022-03-17 18:16:53 +0000

    Added more content and style to the JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,67 @@
-const selectionButtons = document.querySelectorAll('[data-selection]') 
-
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')  
+const SELECTIONS = [
+    {
+    name: 'rock',
+    emoji: '🪨',
+    beats: 'scissors'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'paper'
+        },
+    {
+        name: 'paper',
+        emoji: '📜',
+        beats: 'rock'
+        },
+    ]
 
 selectionButtons.forEach(selectionButtons => {
     selectionButton.addEventListener('click', e => {
         const SelectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === SelectionName)
         makeSelection(SelectionName)
     })
 });
 
 function makeSelection(selection) {
-    console.log(selection);
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
+    
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
+
+    if (yourWinner) incrementScore (yourScoreSpan)
+    if (computerWinner) incrementScore (computerScoreSpan)
 }
+
+function newFunction() {
+    incrementScore(yourScoreSpan)
+}
+
+function incrementScore (scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+const div = document.createElement('div');
+div.innerText = selection.emoji; 
+div.classList.add('result-selection');
+if (isWinner) div.classList.add('winner');
+finalColumn.after(div);
+}
+
+function isWinner (selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomindex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomindex]
+}
+
